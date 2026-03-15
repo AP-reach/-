990a846721ce601cc2d9e3c64c898a0fa73a816d
ShuChenyu <47923686+AP-reach@users.noreply.github.com>
2026-03-15 11:59:42 +0800

    Time: 0 ms (100%), Space: 20.6 MB (51.22%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,36 +1,23 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if root == None:
-            return True
-        def test1(root1: Optional[TreeNode]):
-            if root1 == None:
-                return 0
-            ri = root1.right
-            if ri == None:
-                return root1.val
-            return test1(ri)
-            
-        def test2(root2 : Optional[TreeNode]):
-            if root2 == None:
-                return 0
-            ri = root2.left
-            if ri == None:
-                return root2.val
-            return test2(ri)  
+    def isValidBST(self, root: TreeNode) -> bool:
+        stack, inorder = [], float('-inf')
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            # 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if root.val <= inorder:
+                return False
+            inorder = root.val
+            root = root.right
 
-        lefty = root.left
-        righty = root.right 
-            
-        if self.isValidBST(lefty) and self.isValidBST(righty):
-            if (lefty == None or test1(lefty) < root.val) and ( righty == None or  test2(righty) > root.val):
-                return True
-        return False
+        return True
+
+
+
+
             
 
         
